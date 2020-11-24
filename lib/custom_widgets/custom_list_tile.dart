e124d523045c386867851669fae4b085b9ca28a4
BillyLuisNeedham <billyluisneedham@gmail.com>
2020-11-24 21:17:04 +0000

    percentage working as expected

--- lib/custom_widgets/custom_list_tile.dart
+++ lib/custom_widgets/custom_list_tile.dart
@@ -1,22 +1,24 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:percent_indicator/linear_percent_indicator.dart';
 import 'package:workplace_workout_counter/models/workout.dart';
 
-const _rowHeight = 30.0;
 const _borderRadius = BorderRadius.all(Radius.circular(10));
 
 class CustomListTile extends StatelessWidget {
   final Workout workout;
   final VoidCallback onTap;
-
   CustomListTile({this.workout, this.onTap});
 
   @override
   Widget build(BuildContext context) {
+    final _width = MediaQuery.of(context).size.width - 100;
+    final double percentage = int.parse(workout.dailyReps) / int.parse(workout.remainingReps);
+
+
     return Material(
       color: Colors.transparent,
       child: Container(
-        height: _rowHeight,
         child: InkWell(
           borderRadius: _borderRadius,
           highlightColor: Colors.red[50],
@@ -25,21 +27,42 @@ class CustomListTile extends StatelessWidget {
             print('I was tapped');
             this.onTap();
           },
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    workout.title,
+          child: SizedBox(
+            width: _width,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(10.0, 15.0, 10.0, 10.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        workout.title,
+                      ),
+                      Text(
+                        workout.remainingReps,
+                      )
+                    ],
                   ),
-                  Text(
-                    workout.remainingReps,
-                  )
-                ],
-              ),
-
-            ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 15.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      LinearPercentIndicator(
+                        width: _width,
+                        lineHeight: 12.0,
+                        percent: percentage,
+                        backgroundColor: Colors.grey[300],
+                        progressColor: Colors.grey[500],
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
