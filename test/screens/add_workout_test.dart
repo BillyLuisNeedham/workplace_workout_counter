48fbc92ae0d891583f92c6374c3042e3d8d5867e
BillyLuisNeedham <billyluisneedham@gmail.com>
2020-12-21 17:15:43 +0000

    build out wip

--- test/screens/add_workout_test.dart
+++ test/screens/add_workout_test.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:intl/intl.dart';
+import 'package:workplace_workout_counter/custom_widgets/text_field_standard.dart';
 import 'package:workplace_workout_counter/models/workout.dart';
 import 'package:workplace_workout_counter/screens/add_workout.dart';
 import 'package:workplace_workout_counter/strings.dart';
+import 'package:workplace_workout_counter/utils/database.dart';
 
 void main() {
   group('AddWorkout', () {
@@ -21,7 +24,8 @@ void main() {
       await tester.pump();
 
       //find minutes input and check it exists
-      final finderMinutes = find.widgetWithText(TextField, Strings.minutesPerRep);
+      final finderMinutes =
+          find.widgetWithText(TextField, Strings.minutesPerRep);
       expect(finderMinutes, findsOneWidget);
 
       // tap timer button again
@@ -46,7 +50,8 @@ void main() {
       await tester.pump();
 
       //find seconds input and check exists
-      final finderSeconds = find.widgetWithText(TextField, Strings.secondsPerRep);
+      final finderSeconds =
+          find.widgetWithText(TextField, Strings.secondsPerRep);
       expect(finderSeconds, findsOneWidget);
 
       //tap timer button again
@@ -55,10 +60,29 @@ void main() {
       await tester.pump();
 
       //find no seconds timer input
-          expect(finderSeconds, findsNothing);
+      expect(finderSeconds, findsNothing);
     });
 
-    // TODO change timer tool tip between adding and removing
+    testWidgets(
+        'if the text inputs are empty the add button is clicked, a warning is displayed',
+        (WidgetTester tester) async {
+      Workout testWorkout = new Workout();
+      await tester
+          .pumpWidget(MaterialApp(home: AddWorkout(workout: testWorkout)));
 
+      //tap add button
+      await tester.tap(find.byTooltip(Strings.addButtonToolTip));
+
+      await tester.pump();
+
+      //warning is displayed
+      final finderWarning =
+          find.widgetWithText(AlertDialog, Strings.addWorkoutError);
+      expect(finderWarning, findsOneWidget);
+    });
+
+
+    // TODO change timer tool tip between adding and removing
+    // TODO change reps text input action from next to done when time on or off
   });
 }
